ba11eff1187cec24d261fe00c3276b4e6e06e7d4
swain778 <swain.shub@gmail.com>
2023-12-03 22:20:17 +0530

    setup client and server

--- server/main.go
+++ server/main.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"log"
+	"net"
+
+	pb "gRPC-Demo/proto"
+
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":8000"
+)
+
+type helloServer struct {
+	pb.GreetServiceServer
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("Failed to start the server %v", err)
+	}
+	grpcServer := grpc.NewServer()
+
+	pb.RegisterGreetServiceServer(grpcServer, &helloServer{})
+	log.Printf("server started at %v", lis.Addr())
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Failed to start: %v", err)
+	}
+}
